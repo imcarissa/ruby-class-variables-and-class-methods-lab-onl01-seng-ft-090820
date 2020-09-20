c65d49a313f1a3f70be3cd10461afba7f725a5db
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-20 22:42:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,16 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
+  
  
   def initalize
+    
+    
+  def self.count
+  end
+  
+  def self.genres
+  end
+  
 end
